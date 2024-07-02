795add9466238736539d224af879284abc911eb2
awj625197 <1440382174@qq.com>
2024-07-02 20:53:22 +0800

    Update download_src.sh

--- script/download_src.sh
+++ script/download_src.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install -y python openjdk-8-jdk-headless libncurses5 ccache
 sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
-git clone --depth 1 "https://github.com/kiwibrowser/src" src
+git clone --depth 1 "https://github.com/kiwibrowser/src.next" src
 cd "$ROOT/src"
 echo "MAJOR=112\nMINOR=0\nBUILD=5615\nPATCH=101" > chrome/VERSION
 sudo bash install-build-deps.sh --no-chromeos-fonts
